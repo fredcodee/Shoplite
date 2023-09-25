fa7f22b0035601f4c287298efc07b72696baf5f0
fredcode <fredcode23@gmail.com>
2023-09-26 01:22:15 +0800

    frontend view my products

--- client/src/pages/MyProducts.jsx
+++ client/src/pages/MyProducts.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import DashboardNavBar from '../components/DashboardNavBar'
+import ProductLists from '../components/ProductLists';
 
 const MyProducts = () => {
   return (
-    <div>
+    <div className='container mx-auto pt-3'>
       <DashboardNavBar/>
+      <div className='text-center pt-4'>
+        <button type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Add New Product</button>
+      </div>
+      <div>
+        <h1>My Products</h1>
+        < ProductLists />
+      </div>
     </div>
   )
 }
