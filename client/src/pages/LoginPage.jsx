0692efa3c9aa097a6f05ee5916c9f64d848cdbc0
fredcode <fredcode23@gmail.com>
2023-12-08 15:34:07 +0800

    removed deom accounts feature

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -54,8 +54,7 @@ const LoginPage = () => {
                 handleGoogleAuth(credentialResponse.credential)
               }} />
             </div>
-            <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Demo Accounts</button>
-
+           
             <br />
             <small>Don't have an account yet  <span><a href="/register" className='text-blue-600'>Register here :)</a></span></small>
           </div>
